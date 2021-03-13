34f9f4fa6dcccace250b20b2ff3ae9048ddd3a88
KatyCluckie <krcluckie@gmail.com>
2021-03-13 20:30:55 +0000

    enemy class added, and basic images coming up'

--- movingBin.py
+++ movingBin.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+
 from pygame.locals import *
 from pygame.locals import (
     K_UP,
@@ -44,13 +46,38 @@ class Player(pygame.sprite.Sprite):
         elif self.rect.bottom >= SCREEN_HEIGHT:
             self.rect.bottom = SCREEN_HEIGHT
 
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.image = pygame.image.load('assets/banana.png')
+        self.image.convert()
+        self.image = pygame.transform.scale(self.image, (200, 300))
+        self.surf = pygame.Surface((20, 10))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH -500 , 1000),
+                random.randint(200, SCREEN_HEIGHT),
+            )
+        )
+        self.speed = random.randint(0, 0)
+
+    # Move the sprite based on speed
+    # Remove it when it passes the left edge of the screen
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
 
 pygame.init()
 w, h = 1080, 1000
 screen = pygame.display.set_mode((w, h))
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, 2000)
 running = True
 moving = False
 player = Player()
+enemies = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
@@ -61,12 +88,25 @@ while running:
                 running = False
         elif event.type == QUIT:
             running = False
+        elif event.type == ADDENEMY:
+            new_enemy = Enemy()
+            enemies.add(new_enemy)
+            all_sprites.add(new_enemy)
+
     pressed_keys = pygame.key.get_pressed()
     player.update(pressed_keys)
-
+    enemies.update()
     screen.fill(GRAY)
-    screen.blit(player.image, player.rect)
-    pygame.draw.rect(screen, RED, player.rect, 1)
+
+    for entity in all_sprites:
+        screen.blit(entity.image, entity.rect)
+        pygame.draw.rect(screen, RED, entity.rect, 1)
+
+
+    if pygame.sprite.spritecollideany(player, enemies):
+        player.kill()
+        running = False
+
     pygame.display.update()
 
 pygame.quit()
