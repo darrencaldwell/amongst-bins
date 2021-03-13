0887a4892d2054a3d82f76be1d9460ee7e94c39a
KatyCluckie <krcluckie@gmail.com>
2021-03-13 19:51:52 +0000

    bin moving in class now

--- movingBin.py
+++ movingBin.py
@@ -9,44 +9,50 @@ from pygame.locals import (
     KEYDOWN,
     QUIT,
 )
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-
-def update(pressed_keys):
-    if pressed_keys[K_UP]:
-        rect.move_ip(0, -5)
-    if pressed_keys[K_DOWN]:
-        rect.move_ip(0, 5)
-    if pressed_keys[K_LEFT]:
-        rect.move_ip(-5, 0)
-    if pressed_keys[K_RIGHT]:
-        rect.move_ip(5, 0)
-
-    if rect.left < 0:
-        rect.left = 0
-    elif rect.right > SCREEN_WIDTH:
-        rect.right = SCREEN_WIDTH
-    if rect.top <= 0:
-        rect.top = 0
-    elif rect.bottom >= SCREEN_HEIGHT:
-        rect.bottom = SCREEN_HEIGHT
-
-
+SCREEN_WIDTH = 1080
+SCREEN_HEIGHT = 1000
 RED = (255, 0, 0)
 GRAY = (150, 150, 150)
 
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super(Player, self).__init__()
+        self.image = pygame.image.load('assets/bins/grey bin/grey_left.svg')
+        self.image.convert()
+        self.image = pygame.transform.scale(self.image, (200, 300))
+        self.rect = self.image.get_rect()
+        self.rect.center = w//2, h//2
+
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        elif self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+
 pygame.init()
-w, h = 800, 600
+w, h = 1080, 1000
 screen = pygame.display.set_mode((w, h))
 running = True
-
-img = pygame.image.load('assets/bins/grey bin/grey_left.svg')
-img.convert()
-img = pygame.transform.scale(img, (200, 200))
-rect = img.get_rect()
-rect.center = w//2, h//2
 moving = False
+player = Player()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
 
 while running:
     for event in pygame.event.get():
@@ -56,11 +62,11 @@ while running:
         elif event.type == QUIT:
             running = False
     pressed_keys = pygame.key.get_pressed()
-    update(pressed_keys)
+    player.update(pressed_keys)
 
     screen.fill(GRAY)
-    screen.blit(img, rect)
-    pygame.draw.rect(screen, RED, rect, 1)
+    screen.blit(player.image, player.rect)
+    pygame.draw.rect(screen, RED, player.rect, 1)
     pygame.display.update()
 
 pygame.quit()
